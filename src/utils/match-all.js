763a16dea0ebcdc21cfcb0329a4b317eec1ad755
Márton Visnovitz <vimtaai@inf.elte.hu>
2019-06-11 12:02:03 +0200

    Make matchAll code cleaner

--- src/utils/match-all.js
+++ src/utils/match-all.js
@@ -1,19 +1,22 @@
-function matchAll(str, regex, fn = x => x) {
+function createMatchDescriptor(match) {
+  return {
+    start: match.index,
+    end: match.index + match[0].length,
+    length: match[0].length,
+    content: match[1] || match[0]
+  };
+}
+
+function matchAll(str, regex) {
   const regexClone = new RegExp(regex, "g");
   const results = [];
+
   let match;
-  do {
-    match = regexClone.exec(str);
-    if (match) {
-      const matchDescriptor = {
-        start: match.index,
-        end: match.index + match[0].length,
-        length: match[0].length,
-        content: match[1]
-      };
-      results.push(fn(matchDescriptor));
-    }
-  } while (match);
+  while ((match = regexClone.exec(str))) {
+    const matchDescriptor = createMatchDescriptor(match);
+    results.push(matchDescriptor);
+  }
+
   return results;
 }
 
--- src/utils/match-all.test.js
+++ src/utils/match-all.test.js
@@ -11,6 +11,20 @@ test("`matchAll` should return the proper number of matches", function() {
 
 test("`matchAll` should return the first matching occurence", function() {
   const input = "Lorem ipsum dolor sit ipsum amet...";
+  const regex = / i.+?m /;
+  const expectedOutput = {
+    start: 5,
+    end: 12,
+    length: 7,
+    content: " ipsum "
+  };
+
+  const output = matchAll(input, regex);
+  expect(output[0]).toEqual(expectedOutput);
+});
+
+test("`matchAll` should return the first matching occurence with capture group", function() {
+  const input = "Lorem ipsum dolor sit ipsum amet...";
   const regex = / (i.+?m) /;
   const expectedOutput = {
     start: 5,
@@ -42,33 +56,6 @@ test("`matchAll` sould return all matching occurences", function() {
   ];
 
   const output = matchAll(input, regex);
-  for (let i = 0; i < output.length; i++) {
-    expect(output[i]).toEqual(expectedOutput[i]);
-  }
-});
-
-test("`matchAll` should handle transforming function properly", function() {
-  const input = "Lorem ipsum dolor sit isupm amet...";
-  const regex = / (i.+?m) /;
-  const fn = match => ({ type: "test", ...match });
-  const expectedOutput = [
-    {
-      type: "test",
-      start: 5,
-      end: 12,
-      length: 7,
-      content: "ipsum"
-    },
-    {
-      type: "test",
-      start: 21,
-      end: 28,
-      length: 7,
-      content: "isupm"
-    }
-  ];
-
-  const output = matchAll(input, regex, fn);
   for (let i = 0; i < output.length; i++) {
     expect(output[i]).toEqual(expectedOutput[i]);
   }
